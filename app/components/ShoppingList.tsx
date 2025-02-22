5be4fe9396d0ab3d78c94cc4f0be9d26783d5e14
Camilla <camillamdalan@gmail.com>
2025-02-22 13:55:43 +0100

    improves shopping list calculations and filters out ingredients without quantity

--- app/components/ShoppingList.tsx
+++ app/components/ShoppingList.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { loader as indexLoader } from "~/routes/_index";
+import "core-js/actual";
 
 type ShoppingListProps = {
   calendarData: Awaited<ReturnType<typeof indexLoader>>;
+};
+
+type RequiredShoppingIngredient = {
+  ingredient: string;
+  quantity: number;
+  unitOfMeasure: string;
 };
 
 export const ShoppingList: React.FC<ShoppingListProps> = ({ calendarData }) => {
@@ -12,62 +19,67 @@ export const ShoppingList: React.FC<ShoppingListProps> = ({ calendarData }) => {
     <div className="shadow-md p-4 bg-white rounded-md">
       <h2 className="text-lg font-semibold">Shopping List</h2>
       <div>
-        {Object.entries(shoppingList).map(
-          ([ingredient, { amount, unitOfMeasure }]) => (
-            <div key={ingredient} className="flex gap-2">
-              <input
-                id={ingredient}
-                type="checkbox"
-                key={ingredient}
-                value={`${ingredient} ${amount}${unitOfMeasure}`}
-              />
-              <label htmlFor={ingredient}>
-                {ingredient} {amount} {unitOfMeasure}
-              </label>
-            </div>
-          )
-        )}
+        {shoppingList.map(({ ingredient, quantity, unitOfMeasure }) => (
+          <div key={ingredient} className="flex gap-2">
+            <input
+              id={ingredient}
+              type="checkbox"
+              key={ingredient}
+              value={`${ingredient} ${quantity}${unitOfMeasure}`}
+            />
+            <label htmlFor={ingredient}>
+              {ingredient} {quantity} {unitOfMeasure}
+            </label>
+          </div>
+        ))}
       </div>
     </div>
   );
 };
 
-function getShoppingList(result: Awaited<ReturnType<typeof indexLoader>>) {
-  const ingredientsWithQuantity: Record<
-    string,
-    { amount: number; unitOfMeasure: string | null }
-  > = {};
+function isRequiredIngredient(i: unknown): i is RequiredShoppingIngredient {
+  return (
+    typeof i === "object" &&
+    i != null &&
+    "ingredient" in i &&
+    i.ingredient !== null &&
+    "quantity" in i &&
+    i.quantity !== null &&
+    "unitOfMeasure" in i &&
+    i.unitOfMeasure !== null
+  );
+}
 
-  result.forEach((date) => {
-    return Object.values(date.meals).forEach((meal) => {
-      if (!meal) {
-        return [];
-      }
+function getShoppingList(
+  result: Awaited<ReturnType<typeof indexLoader>>
+): RequiredShoppingIngredient[] {
+  const meals = result.flatMap((date) => Object.values(date.meals));
 
-      for (const ingredient of meal.ingredients) {
-        const ingredientName = ingredient.ingredient;
-        if (!ingredientName) {
-          continue;
-        }
+  const ingredients = meals.flatMap((meal) =>
+    meal?.ingredients
+      .filter((it) => isRequiredIngredient(it))
+      .map((i) => ({
+        ...i,
+        servings: meal.servings ?? 0,
+      }))
+  );
 
-        if (!ingredientsWithQuantity[ingredientName]) {
-          ingredientsWithQuantity[ingredientName] = {
-            amount: 0,
-            unitOfMeasure: null,
-          };
-        }
+  const ingredientsWQuantity = Object.values(
+    Object.groupBy(ingredients, (i) => i.ingredient ?? "")
+  )
+    .map((ingredientEntries) =>
+      ingredientEntries?.reduce<RequiredShoppingIngredient>(
+        (acc, curr) => ({
+          ...acc,
+          ingredient: curr.ingredient ?? acc.ingredient,
+          unitOfMeasure: curr.unitOfMeasure ?? acc.unitOfMeasure,
+          quantity: (acc.quantity ?? 0) + curr.quantity * curr.servings,
+        }),
+        {} as RequiredShoppingIngredient
+      )
+    )
+    .filter((it) => it != null)
+    .filter((it) => it.quantity > 0);
 
-        const quantity = ingredient.quantity ?? 0;
-        const servings = meal.servings ?? 1;
-        ingredientsWithQuantity[ingredientName].amount += quantity * servings;
-
-        if (ingredient.unitOfMeasure) {
-          ingredientsWithQuantity[ingredientName].unitOfMeasure =
-            ingredient.unitOfMeasure;
-        }
-      }
-    });
-  });
-
-  return ingredientsWithQuantity;
+  return ingredientsWQuantity;
 }
